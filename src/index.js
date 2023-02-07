d2b78da6d0bb519c282e107aa87c58c132be9525
Vinh Nguyen <nguyenlebaovinh.27@gmail.com>
2023-02-07 09:48:49 -0600

    back to Lisa

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 //Hi everyone!!!
 
 let addToy = false;
-const toyCollection = document.querySelector('#toy-collection')
+
 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
@@ -19,24 +19,19 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 //Use fetch() to make a "GET" request, then render the returned toys to the DOM
+function fetchImage() {
+  fetch ("http://localhost:3000/toys")
+    .then(r => r.json)
+    .then(data => {
+      console.log(data)
+      renderImage(data)
+    })
+}
+fetchImage()
 
-fetch ("http://localhost:3000/toys")
-  .then(r => r.json)
-  .then(data => {
-    console.log(data)
-    renderImage(data)
-  })
-
-
-
-// {
-//   "id": 1,
-//   "name": "Woody",
-//   "image": "http://www.pngmart.com/files/3/Toy-Story-Woody-PNG-Photos.png",
-//   "likes": 5
-// },
 //creating function renderImage
 function renderImage(image) {
+  const toyCollection = document.querySelector('#toy-collection')
   //image.forEach(image => {
     const li = document.createElement('li')
     li.className = 'list-li'
@@ -50,9 +45,11 @@ function renderImage(image) {
     img.alt = `${image.name} cover`
     li.append(img)
 
-    const like = document.createElement('p')
-    
+    const pLike = document.createElement('p')
+    pLike.textContent = image.likes
+    li.append(pLike)
 
+    toyCollection.append(li)
 
   //})
 }
